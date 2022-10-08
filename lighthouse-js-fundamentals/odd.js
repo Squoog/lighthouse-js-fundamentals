cb0d68a38736d8f482219a3708869d092d0e198a
Ryan Zhen <rjhzhen@gmail.com>
2022-10-08 19:07:26 -0400

    isOdd function

---
+++ lighthouse-js-fundamentals/odd.js
@@ -0,0 +1,11 @@
+function isOdd(n){
+    if (n % 2 === 1){
+        return false
+    }
+    else{
+        return true
+    }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
